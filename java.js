364a40ecfc42ef1d938406def78fd2c1f5b929d6
zairasssilveira <zaira.salomao12@gnail.com>
2023-08-07 09:13:05 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,31 @@
+
+        // // Função para salvar o cadastro no console
+        // function salvarCadastro(event) {
+        //     event.preventDefault(); // Evita o comportamento padrão do formulário
+
+        //     // Obtém os valores dos campos do formulário
+        //     const nome = document.getElementById('nome').value;
+        //     const sobrenome = document.getElementById('sobrenome').value;
+        //     const email = document.getElementById('email').value;
+
+        //     // Cria um objeto com os dados do cadastro
+        //     const cadastro = {
+        //         nome: nome,
+        //         sobrenome: sobrenome,
+        //         email: email
+        //     };
+
+        //     // Exibe o cadastro no console
+        //     console.log(cadastro);
+
+        //     // Limpa os campos do formulário
+        //     document.getElementById('Nome:').value = '';
+        //     document.getElementById('Sobrenome:').value = '';
+        //     document.getElementById('Email:').value = '';
+        // }
+
+        // // Adiciona um ouvinte de evento para o envio do formulário
+        // const formulario = document.getElementById('formulario');
+        // formulario.addEventListener('submit', salvarCadastro);
+
+                                                                                                                                                                                                                                                               
